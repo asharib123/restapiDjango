964608298f7b8e4807ca6bfd9018663c4c15b904
Asharib Kamal <asharibkamal1@gmail.com>
2020-11-04 15:52:49 +0500

    new change

--- todo/settings.py
+++ todo/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    ''
     'django.contrib.staticfiles',
     'api.apps.apiConfig',
     'rest_framework',
